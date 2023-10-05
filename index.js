a465bb4e429765b312e707364fbcb158d3472bc6
K Santhosh <87223844+Ksanthosh20ka1a0554@users.noreply.github.com>
2023-10-05 10:49:05 +0530

    8 commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const admin = require('firebase-admin');
 const serviceAccount = 'i-have-food-e4a0d-firebase-adminsdk-w1f1j-d737c84b53.json';
 
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://i-have-food-e4a0d-default-rtdb.asia-southeast1.firebasedatabase.app"
@@ -11,28 +10,33 @@ admin.initializeApp({
 const db = admin.database();
 
 const app = express();
-const port = process.env.PORT || 7000; // Change this to your desired port
+const port = process.env.PORT || 7000;
 
 app.use(express.json());
 
 app.post('/foodLocationChangeRequest', (req, res) => {
-    var currentUserId = req.body.userId;
+    const currentUserId = req.body.userId;
 
-    // Fetch FCM tokens from FCMTokens child
     const fcmTokensRef = db.ref('FCMTokens');
     fcmTokensRef.once('value')
         .then(tokensSnapshot => {
+            const promises = [];
+
             tokensSnapshot.forEach(tokenSnapshot => {
-                var deviceFcmToken = tokenSnapshot.val(); // Assuming each child node under FCMTokens is a token
-                sendFCMNotification(deviceFcmToken)
-                
-                // Skip sending notification to the current user
-                // if (tokenSnapshot.key !== currentUserId) {
-                    
-                // }
+                const deviceFcmToken = tokenSnapshot.val();
+                const promise = sendFCMNotification(deviceFcmToken);
+                promises.push(promise);
             });
 
-            res.status(200).send('Food location updated and notifications sent');
+            // Wait for all promises to resolve
+            Promise.all(promises)
+                .then(() => {
+                    res.status(200).send('Food location updated and notifications sent');
+                })
+                .catch(error => {
+                    console.error('Error sending FCM messages:', error.message);
+                    res.status(500).send('Internal Server Error');
+                });
         })
         .catch(error => {
             console.error('Error fetching FCM tokens:', error.message);
@@ -41,7 +45,7 @@ app.post('/foodLocationChangeRequest', (req, res) => {
 });
 
 function sendFCMNotification(deviceFcmToken) {
-    var message = {
+    const message = {
         data: {
             title: 'New Food Donation',
             body: 'There is a new food donation available near you.'
@@ -49,12 +53,15 @@ function sendFCMNotification(deviceFcmToken) {
         token: deviceFcmToken
     };
 
-    admin.messaging().send(message)
-        .then((response) => {
+    // Return the promise
+    return admin.messaging().send(message)
+        .then(response => {
             console.log('Successfully sent FCM message:', response);
         })
-        .catch((error) => {
+        .catch(error => {
             console.error('Error sending FCM message:', error);
+            // Propagate the error to be caught by the calling function
+            throw error;
         });
 }
 
